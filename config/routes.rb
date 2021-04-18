71a0752c3aed753fb38ad81945383c338de93d40
michi <miti25@users.noreply.github.com>
2021-04-19 00:01:47 +0900

    UserSession作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
+
+  namespace :admin do
+    resources :users
+  end
+
   root 'pages#home'
   resources :recipes
 end
