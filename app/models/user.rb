41a0c5a493dc5765d5d2b857106c2e6ba21aa084
michi <miti25@users.noreply.github.com>
2021-04-21 14:21:46 +0900

    ユーザー削除時レシピ削除作動

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
 
-  has_many :recipes
+  has_many :recipes, dependent: :destroy
 end
