3143dea4857df66289bf4859ce9cec2bf5692d10
michi <miti25@users.noreply.github.com>
2021-05-13 23:34:46 +0900

    minimagick導入に伴う画像表示等変更

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,16 +1,19 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, length: { maximum: 200 }
-
   belongs_to :user
-
+  has_one_attached :image
   scope :recent, -> { order(created_at: :desc) }
+
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[name]
   end
-
   def self.ransackable_associations(_auth_object = nil)
     []
+  end
+
+  def thumbnail
+    return self.image.variant(resize: '200x300').processed
   end
 end
