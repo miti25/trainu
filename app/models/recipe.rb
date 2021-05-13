40bcceb5eb9fdb69b8facb2346652924701b22d7
michi <miti25@users.noreply.github.com>
2021-05-13 23:52:24 +0900

    不要なクラス指定削除

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,15 +5,15 @@ class Recipe < ApplicationRecord
   has_one_attached :image
   scope :recent, -> { order(created_at: :desc) }
 
-
   def self.ransackable_attributes(_auth_object = nil)
     %w[name]
   end
+
   def self.ransackable_associations(_auth_object = nil)
     []
   end
 
   def thumbnail
-    return self.image.variant(resize: '200x300').processed
+    image.variant(resize: '200x300').processed
   end
 end
