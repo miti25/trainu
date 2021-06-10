96f31b7ac755361c70cd9817a01b50c4608cfa81
michi <miti25@users.noreply.github.com>
2021-06-10 15:04:16 +0900

    howto画像選択時の自動保存実装

--- app/assets/javascripts/recipe.js
+++ app/assets/javascripts/recipe.js
@@ -21,14 +21,19 @@ document.addEventListener('turbolinks:load', function() {
       }
     });
   document.querySelectorAll('.howto_uploader').forEach(function(a) {
-    a.addEventListener('change', function() {
-      const file = a.files[0];
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-      reader.onload = function() {
-        const image = reader.result;
-        document.querySelector('.howto_preview').setAttribute('src', image);
+    a.addEventListener('change', function(e) {
+      const target = document.querySelector('.update_btn')
+      console.log(target.className)
+      if (e.target.files[0].size > 0) {
+        $(this).parents('form').submit();
       }
+    });
+  });
+  const howtos = Array.from(document.querySelectorAll('.howto_uploader'));
+  howtos.forEach(function(h) {
+    h.addEventListener('click', function(e) {
+      const index = howtos.findIndex(howto => howto === e.target);
+      console.log(index);
     });
   });
 });
