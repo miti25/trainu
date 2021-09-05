859a014b4f57d1b343cd80c8a6e905a7c5dfb028
michi <miti25@users.noreply.github.com>
2021-09-05 16:12:49 +0900

    定数の書き換え予防

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :name, presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   has_many :recipes, dependent: :destroy
   has_many :favorites, dependent: :destroy
