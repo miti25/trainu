67acaec6b368fbc4613ce945df5993a77fb8e048
michi <miti25@users.noreply.github.com>
2021-09-07 15:22:42 +0900

    ログインを要求する際の注意書きを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   private
 
   def login_required
-    redirect_to login_path unless current_user
+    redirect_to login_path, notice: 'ログインしてください' unless current_user
   end
 
   def set_search
