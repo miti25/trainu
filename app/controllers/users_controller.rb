0a1e1c8fb90f95f318dfed5a8630afb3d20fbb09
michi <miti25@users.noreply.github.com>
2022-02-22 14:22:49 +0900

    ユーザーページにお気に入りが表示されるように

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @recipes = @user.recipes.includes([:recipe_categories, :categories]).with_attached_image.recent
+    @favorite_recipes = @user.favorite_recipes
   end
 
   def new
